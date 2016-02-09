81eb658f7d342a1f55e73885472c4f02d111eba2
sawai takahiro <narcissis.jiteki+git@gmail.com>
2016-02-09 23:34:32 +0900

    レース条件を抜き出して足す処理を追加。
    とりあえずで、上がり３Fを求めるスクリプトに足した。
    こんな感じで取得する処理が増えていくはずなので、書き方を整理したいな。

--- add_last3f.rb
+++ add_last3f.rb
@@ -19,9 +19,16 @@ require "./library.rb"
 db = SQLite3::Database.new("./source/keiba_data.db")	#原本のデータベースを開く
 db.execute('attach "./source/localdata.db" as local')	#コピー先のデータベースをアタッチ
 
+#上がり３Fを抜き出す
 path_file_query = "./query/query_add_last3f.txt"
 query = File.read(path_file_query)
 db.execute(query)
+
+#レースの条件も足しちゃう
+path_file_query = "./query/query_add_joken.txt"
+query = File.read(path_file_query)
+db.execute(query)
+
 
 db.execute('detach database local')	#コピー先のデータベースを外す！
 db.close	#データベースを閉じる
